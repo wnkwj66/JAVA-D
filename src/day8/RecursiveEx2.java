cffbc8d8b270adee23abfa4ab80fe16226c4c6c1
KWJ <wnkwj66@naver.com>
2020-04-28 16:34:31 +0900

    8일차 -메서드 재귀호출 예제

---
+++ src/day8/RecursiveEx2.java
@@ -0,0 +1,18 @@
+package day8;
+
+public class RecursiveEx2 {
+
+	public static void main(String[] args) {
+		int num = 5;
+		int res = 1;
+		for(int i=1; i<=num; i++) {
+			res *= i; //rse = rse * i;
+		}
+		if(num <0)
+			System.out.println(0);
+		else {
+			System.out.println(res);
+		}
+	}
+
+}
