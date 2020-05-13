3a79e63bad53685a4785d615b4c9646f5c8a16bb
KWJ <wnkwj66@naver.com>
2020-05-13 17:28:14 +0900

    15일차-Queue 예제

---
+++ src/day15/QueueEx1.java
@@ -0,0 +1,25 @@
+package day15;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueEx1 {
+
+	public static void main(String[] args) {
+
+		Queue<Integer> q = new LinkedList<Integer>();
+		q.offer(1);
+		q.offer(2);
+		q.offer(3);
+		System.out.println(q.poll()+"을 큐에서 꺼냈습니다.");
+		System.out.println(q.poll()+"을 큐에서 꺼냈습니다.");
+		System.out.println(q.poll()+"을 큐에서 꺼냈습니다.");
+//		q.poll();
+//		큐는 LinkedList로 만들었고, 이때 offer는 마지막 객체 뒤에다 새로운 객체정보를 저장한다.
+//		poll은 0번지의 값을 제거하고 반환한다.
+		for(Integer tmp : q) {
+			System.out.println(tmp);
+		}
+	}
+
+}
